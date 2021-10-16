8f324d90e6af32759038b091096b727bf1bad6a6
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-10-16 03:05:44 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import NoteForm from './components/NoteForm';
-import Note from './components/note';
+import NoteForm from './components/noteForm';
+import Notes from './components/notes';
 
 import Modal from 'react-modal';
 import swal from 'sweetalert';
@@ -24,19 +24,15 @@ const customStyles = {
   }
 };
 
-function App(props) {
-  // localforage.clear()
-  let localforageNotes = { active: [], archived: [] };
-
-  const [notes, setNotes] = useState(localforageNotes);
+function App() {
+  const [notes, setNotes] = useState({ active: [], archived: [] });
   const [modalIsOpen, setIsOpen] = useState(false);
   const [modalNoteIndex, setModalNoteIndex] = useState(null);
 
   useEffect(() => {
     async function getLocalforageNotes() {
       const getNotes = await localforage.getItem('notes');
-      localforageNotes = (getNotes) ? getNotes : { active: [], archived: [] };
-      setNotes(localforageNotes);
+      setNotes((getNotes) ? getNotes : { active: [], archived: [] });
     }
 
     getLocalforageNotes();
@@ -51,11 +47,16 @@ function App(props) {
 
   const handleUpdate = async (updatedNote) => {
     let updatedActive = [...notes.active];
-    updatedActive.splice(modalNoteIndex, 1, updatedNote);
-    updatedActive = sortByDate(updatedActive);
-    const updatedNotes = { ...notes, ...{ active: updatedActive } };
-    setNotes(updatedNotes);
-    await localforage.setItem('notes', updatedNotes);
+    const reminderChanged = (updatedActive[modalNoteIndex].reminder !== updatedNote.reminder) ? true : false;
+    if ((updatedActive[modalNoteIndex].title !== updatedNote.title) ||
+        (updatedActive[modalNoteIndex].text !== updatedNote.text) ||
+        reminderChanged) {
+      updatedActive.splice(modalNoteIndex, 1, updatedNote);
+      updatedActive = (!reminderChanged) ? sortByDate(updatedActive) : updatedActive;
+      const updatedNotes = { ...notes, ...{ active: updatedActive } };
+      setNotes(updatedNotes);
+      await localforage.setItem('notes', updatedNotes);
+    }
     closeModal();
   }
 
@@ -91,15 +92,12 @@ function App(props) {
         updatedArchived = sortByDate(updatedArchived);
         
         handleToggle(updatedActive, updatedArchived);
-      } else {
-        swal(`${(isArchived) ? `Restore` : `Archive`} cancelled`);
-        return;
-      }
+      } else swal(`${(isArchived) ? `Restore` : `Archive`} cancelled`);
     });
   }
 
-  const openModal = (e, updateIndex, isArchived) => {
-    if ((e.target.classList.contains('fa-trash')) || isArchived) return;
+  const openModal = (targetElement, updateIndex, isArchived) => {
+    if ((targetElement.classList.contains('fa-trash')) || isArchived) return;
     setModalNoteIndex(updateIndex);
     setIsOpen(true);
   }
@@ -108,68 +106,36 @@ function App(props) {
     setIsOpen(false);
   }
 
+  const checkReminder = () => {
+    let updatedActive = [...notes.active];
+    updatedActive.forEach((note, index) => {
+      if ((note.reminder) && (Date.parse(note.reminder) <= (new Date()))) {
+        swal("Note Reminder!", "", "info", { button: "Show Note" })
+        .then(() => {
+          note.reminder = '';  
+          openModal(document.querySelector('.notes'), index, false);
+          const updatedNotes = { ...notes, ...{ active: updatedActive } };
+          setNotes(updatedNotes);
+          return;
+        });
+      }
+    });
+  }
+
+  setInterval(function() { checkReminder() }, 2 * 60 * 1000);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-h1">Notes Cork Board with React.js</h1>
-        <NoteForm
-          onAdd={handleAdd}
-          title={''}
-          text={''} />
+        <NoteForm onAdd={handleAdd} />
       </header>
       <main className="App-main">
-        <section>
-          <div className="notes">
-            {notes.active.length ?
-            notes.active.map((note, index) =>
-              <Note
-                key={note.id}
-                index={index}
-                onToggleArchive={confirmToggle}
-                isArchived={false}
-                onOpenModal={openModal}
-                title={note.title}
-                createdAt={note.createdAt}
-                updatedAt={note.updatedAt}
-                text={note.text}
-                id={note.id} />) :
-            <p className="text-bg">No active notes</p>}
-          </div>
-          <h2 className="text-bg">Active Notes</h2>
-        </section>
-        <section>
-          <div className="notes">
-            {notes.archived.length ?
-              notes.archived.map((note, index) =>
-                <Note
-                  key={note.id}
-                  index={index}
-                  onToggleArchive={confirmToggle}
-                  isArchived={true}
-                  onOpenModal={openModal}
-                  title={note.title}
-                  createdAt={note.createdAt}
-                  updatedAt={note.updatedAt}
-                  text={note.text}
-                  id={note.id} />) :
-              <p className="text-bg">No archived notes</p>}
-            </div>
-            <h2 className="text-bg">Archived Notes</h2>
-        </section>
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Note Modal"
-        >
+        <Notes notes={notes.active} {...{ confirmToggle, openModal }} />
+        <Notes notes={notes.archived} {...{ confirmToggle, openModal }} isArchived />
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
           <span className="modal__item modal__item--close" onClick={closeModal}>✖</span>
-          <NoteForm
-            onUpdate={handleUpdate}
-            id={modalIsOpen ? notes.active[modalNoteIndex].id : null}
-            title={modalIsOpen ? notes.active[modalNoteIndex].title : null}
-            text={modalIsOpen ? notes.active[modalNoteIndex].text : null}
-            createdAt={modalIsOpen ? notes.active[modalNoteIndex].createdAt : null}
-            updatedAt={modalIsOpen ? notes.active[modalNoteIndex].updatedAt : null} />
+          <NoteForm onUpdate={handleUpdate} existingNote={modalIsOpen ? notes.active[modalNoteIndex] : null} />
         </Modal>
 
       </main>
--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,48 +1,66 @@
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-function NoteForm(props) {
+function NoteForm({ onAdd, onUpdate, existingNote }) {
 
-    const [title, setTitle] = useState(props.title);
-    const [text, setText] = useState(props.text);
+    const [noteInputs, setNoteInputs] = useState(existingNote ?
+        { title: existingNote.title, text: existingNote.text, reminder: existingNote.reminder }
+        :
+        { title: '', text: '', reminder: '' });
 
     useEffect(() => {
         adjustTextareaHeight();
     });
 
     const handleNoteTitle = (e) => {
-        setTitle(e.target.value);
+        setNoteInputs({ ...noteInputs, title: e.target.value });
+        hideShowLegend(e.target);
     }
 
     const handleNoteText = (e) => {
-        setText(e.target.value);
+        setNoteInputs({ ...noteInputs, text: e.target.value });
+        hideShowLegend(e.target);
         adjustTextareaHeight();
     }
 
+    const handleNoteReminder = (e) => setNoteInputs({ ...noteInputs, reminder: e.target.value });
+
     const handleNote = (e) => {
         e.preventDefault();
-        const noteId = (props.id) ? props.id : uuidv4();
-        const noteCreatedAt = (props.createdAt) ? props.createdAt : new Date();
-        const noteUpdatedAt = (props.createdAt) ? new Date() : null;
-        const note = { title, text, createdAt: noteCreatedAt, updatedAt: noteUpdatedAt, id: noteId }
-        if (props.id) props.onUpdate(note);
-        else props.onAdd(note);
-        setTitle('');
-        setText('');
+        const noteId = (existingNote) ? existingNote.id : uuidv4();
+        const noteCreatedAt = (existingNote) ? existingNote.createdAt : new Date();
+        const noteUpdatedAt = (existingNote) ? new Date() : null;
+        const note = { title: noteInputs.title, text: noteInputs.text, reminder: noteInputs.reminder, createdAt: noteCreatedAt, updatedAt: noteUpdatedAt, id: noteId }
+        if (existingNote) onUpdate(note);
+        else onAdd(note);
+        setNoteInputs({ title: '', text: '', reminder: '' });
     }
 
     const adjustTextareaHeight = () => {
-        const textarea = document.querySelector('.note-form__item--text');
+        const textarea = document.querySelector('.input-text');
         textarea.style.height = "auto";
         textarea.style.height = (textarea.scrollHeight) + "px";
     }
 
+    const hideShowLegend = (input) => input.previousElementSibling.style.display = (input.value) ? "unset" : "none";
+
+    const dateToIsoStringWithTZ = (date) => new Date(date.getTime() - ((new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, 16).replace(' ','T');
+
     return (
         <form onSubmit={(e) => {handleNote(e)}}  className='note-form'>
-            <input placeholder='Note Title (max 25 chars)' value={title} onChange={handleNoteTitle} maxLength={25} className='note-form__item note-form__item--title'/>
-            <textarea placeholder='Note text (max 250 chars)' value={text} onChange={handleNoteText} maxLength={250} className='note-form__item note-form__item--text' required>
-                
-            </textarea>
+            <fieldset className='note-form__item note-form__item--title'>
+                <legend className='legend-title'>Note Title</legend>
+                <input placeholder='Note Title (max 25 chars)' value={noteInputs.title} onChange={handleNoteTitle} maxLength={25} className='input-title'/>
+            </fieldset>
+            <fieldset className='note-form__item note-form__item--text'>
+                <legend className='legend-text'>Note Text</legend>
+                <textarea placeholder='Note Text (max 250 chars)' value={noteInputs.text} onChange={handleNoteText} maxLength={250} className='input-text' required>
+                </textarea>
+            </fieldset>
+            <fieldset className='note-form__item note-form__item--reminder'>
+                <label className="label-reminder">Reminder</label>
+                <input type="datetime-local" value={noteInputs.reminder} onChange={handleNoteReminder} min={dateToIsoStringWithTZ(new Date())} className='input-reminder' />
+            </fieldset>
             <input type='submit' value='Done' className='note-form__item note-form__item--submit' />
         </form>
     );
